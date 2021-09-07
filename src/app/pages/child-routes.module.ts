f3bfab3a91227690ca4e1abce964d367ac806cda
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-07 11:04:55 -0600

    implementando routerLink con id a reporte-empleado.component

--- src/app/pages/child-routes.module.ts
+++ src/app/pages/child-routes.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { ListadoEmpleadoComponent } from './empleado/listado-empleado/listado-empleado.component';
+import { ReporteEmpleadoComponent } from './empleado/reporte-empleado/reporte-empleado.component';
 import { PruebaComponent } from './prueba/prueba.component';
 
 const childRoutes: Routes = [
   { path: 'listado', component: ListadoEmpleadoComponent },
   { path: 'empleado', component: EmpleadoComponent },
+  { path: 'actualizar/:id', component: ReporteEmpleadoComponent},
   { path: 'main', component: DashboardComponent },
 ]
 @NgModule({
